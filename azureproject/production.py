0f59cd48efc045717b4e69916f68e3943730288e
ShayBar1 <117023790+ShayBar1@users.noreply.github.com>
2022-10-30 16:13:58 +0200

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -4,14 +4,14 @@ import os
 SECRET_KEY = 'flask-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
 
 DEBUG = False
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['shaybar.azurewebsites.net']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['shaybar.azurewebsites.net']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
 # username (not @sever-name).
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'] + ".postgres.database.azure.com",
-    dbname=os.environ['DBNAME']
+    dbuser=os.environ['apguauzqmd'],
+    dbpass=os.environ['SJT3A7GIPZRP133C$'],
+    dbhost=os.environ['shaybar-server'] + ".postgres.database.azure.com",
+    dbname=os.environ['postgres']
 )
